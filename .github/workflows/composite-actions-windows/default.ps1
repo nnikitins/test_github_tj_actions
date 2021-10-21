afe6ee247ceadf6eedf1bc18a0ff6e845aaaa0d9
NNikitins <91458178+NNikitins@users.noreply.github.com>
2021-10-21 14:50:35 +0300

    Update default.ps1

--- .github/workflows/composite-actions-windows/default.ps1
+++ .github/workflows/composite-actions-windows/default.ps1
@@ -1,1 +1,9 @@
 param($github_ref, $current_branch, $ref_branch)
+
+if ("${github_ref}" -ne "refs/tags/"*) {
+  if ("${current_branch}" -eq "${ref_branch}" ) {
+    Write-Output "::set-env name=is_default::true"
+   } else {
+    Write-Output "::set-env name=is_default::false"
+   }
+}
