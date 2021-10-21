5ae8a4f778f64a749b3d5a058056159c48a6a045
Sinet2000 <giinger11352@gmail.com>
2021-10-21 14:56:40 +0300

    Update tag.ps1

--- .github/workflows/composite-actions-windows/tag.ps1
+++ .github/workflows/composite-actions-windows/tag.ps1
@@ -1,1 +1,10 @@
 param($github_ref, $strip_tag_prefix)
+
+if ("${github_ref}" -eq "refs/tags/") {
+	$REF="${github_ref}"
+	$TAG=${$REF/refs\/tags\/$strip_tag_prefix/}
+	Write-Output "::set-env name=tag::$TAG"
+	Write-Output "::set-env name=is_tag::true"
+} else {
+	Write-Output "::set-env name=is_tag::false"
+}
