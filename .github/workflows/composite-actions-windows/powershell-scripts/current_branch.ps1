605d5f858489a5a08ad64084472033ac1ec0c22e
NNikitins <91458178+NNikitins@users.noreply.github.com>
2021-10-22 00:42:01 -0700

    Update current_branch.ps1

--- .github/workflows/composite-actions-windows/powershell-scripts/current_branch.ps1
+++ .github/workflows/composite-actions-windows/powershell-scripts/current_branch.ps1
@@ -1,27 +1,31 @@
 param ($github_ref, 
-	[Switch]
+	[Parameter(Mandatory=$false)]
+  	[Switch]
 	$github_event_name, 
-	[Switch]
+
+	[Parameter(Mandatory=$false)]
+  	[Switch]
 	$head_ref_branch,
-	[Switch]
+
+	[Parameter(Mandatory=$false)]
+  	[Switch]
 	$ref_branch, 
-	[Switch]
+	
+	[Parameter(Mandatory=$false)]
+  	[Switch]
 	$strip_tag_prefix
 )
 
 if ( "${github_ref}" -ne "refs/tags/") {
 	if ($github_event_name -eq 'pull_request' ) {
 		echo "::set-output name=current_branch::$head_ref_branch"
-		#Write-Output "::set-env name=current_branch::$head_ref_branch"
 	}
- else {
+ 	else {
 		echo "::set-output name=current_branch::$ref_branch"
-		#Write-Output "::set-env name=current_branch::$ref_branch"
 	}
 }
 else {
 	$REF = "${github.ref }"
 	$REF_BRANCH = ${$REF/refs\/tags\/$strip_tag_prefix/}
 	echo "::set-output name=current_branch::"${REF_BRANCH"}""
-	#Write-Output "::set-env name=current_branch::"${REF_BRANCH"}""
 }
