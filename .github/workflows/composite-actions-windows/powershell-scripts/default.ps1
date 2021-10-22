ac922f99f97cbf364eaf675f8cb7e28aa8652aae
NNikitins <91458178+NNikitins@users.noreply.github.com>
2021-10-22 00:42:49 -0700

    Update default.ps1

--- .github/workflows/composite-actions-windows/powershell-scripts/default.ps1
+++ .github/workflows/composite-actions-windows/powershell-scripts/default.ps1
@@ -1,18 +1,19 @@
 param($github_ref, 
-	[Switch]
+	
+	[Parameter(Mandatory=$false)]
+  	[Switch]
 	$current_branch, 
-	[Switch]
+	
+	[Parameter(Mandatory=$false)]
+  	[Switch]
 	$ref_branch
 )
 
 if ("${github_ref}" -ne "refs/tags/") {
 	if ("${current_branch}" -eq "${ref_branch}" ) {
 		echo "::set-output name=is_default::true"
-		
-		#Write-Output "::set-env name=is_default::true"
 	}
- else {
+ 	else {
 		echo "::set-output name=is_default::false"
-		#Write-Output "::set-env name=is_default::false"
 	}
 }
