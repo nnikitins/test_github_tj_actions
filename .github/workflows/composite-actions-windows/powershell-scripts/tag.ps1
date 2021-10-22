067791354047c8c3929eb7e3d4ad56efbcf0f005
NNikitins <91458178+NNikitins@users.noreply.github.com>
2021-10-22 00:43:15 -0700

    Update tag.ps1

--- .github/workflows/composite-actions-windows/powershell-scripts/tag.ps1
+++ .github/workflows/composite-actions-windows/powershell-scripts/tag.ps1
@@ -1,5 +1,7 @@
 param($github_ref, 
-	[Switch]
+	
+	[Parameter(Mandatory=$false)]
+  	[Switch]
 	$strip_tag_prefix
 )
 
@@ -7,14 +9,9 @@ if ("${github_ref}" -eq "refs/tags/") {
 	$REF = "${github_ref}"
 	$TAG = ${$REF/refs\/tags\/$strip_tag_prefix/}
 
-	#are disabled for sec reasons
-	#Write-Output "::set-env name=tag::$TAG"
-	#Write-Output "::set-env name=is_tag::true"
-
 	echo "::set-output name=tag::$TAG"
 	echo "::set-output name=is_tag::true"
 }
 else {
 	echo "::set-output name=is_tag::false"
-	#Write-Output "::set-env name=is_tag::false"
 }
